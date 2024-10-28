b5c2c384fe17637a7f6823fa798e511893cac089
Khaledsherif2 <khaled.dherif220@gmail.com>
2024-10-28 18:12:27 +0300

    updates

--- src/Profile/Profile.jsx
+++ src/Profile/Profile.jsx
@@ -1,7 +1,7 @@
 import "./Profile.css";
 import "../CSS/framework.css";
 import NavBar from "../NavBar/NavBar";
-import SideBar from "../Sidebar/SideBar";
+import SideBar from "../Sidebar/Sidebar";
 
 const Profile = () => {
   return (
